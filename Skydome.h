2a10a6609560f94bd151bde0927270268728078a
美鈴 武田 <k022g1076@m.neec.ac.jp>
2023-06-05 14:15:07 +0900

    skydome.hの生成

---
+++ Skydome.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include "Model.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+/// <summary>
+/// 天球クラス
+/// </summary>
+class Skydome {
+
+public:
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+
+	/// <summary>
+	/// 描写
+	/// </summary>
+	void Draw();
+
+
+private:
+
+	///ワールド変換データ
+	WorldTransform worldTransform_;
+
+	//モデルデータ
+	Model* model_ = nullptr;
+
+};
