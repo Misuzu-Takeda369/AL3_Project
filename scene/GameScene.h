a45c03eddbc4c4366fea7af3509d90ddd1ddbd52
美鈴 武田 <k022g1076@m.neec.ac.jp>
2023-06-26 16:03:38 +0900

    修正

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -3,18 +3,19 @@
 #include "Audio.h"
 #include "DebugCamera.h"
 #include "DirectXCommon.h"
+#include "Enemy.h"
+#include "EnemyBullet.h"
 #include "Input.h"
 #include "Model.h"
 #include "Player.h"
+#include "RailCamera.h"
 #include "SafeDelete.h"
+#include "Skydome.h"
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-#include "Enemy.h"
-#include "Skydome.h"
-#include "RailCamera.h"
 #include <list>
-
+#include <sstream>
 
 /// <summary>
 /// ゲームシーン
@@ -53,14 +54,28 @@ public: // メンバ関数
 	void CheckAllCollisions();
 
 	/// <summary>
-	/// 敵の球を追加する(エネミークラスにあったやつをシーンに持ち込む)
+	/// ゲームシーンに球を登録する
 	/// </summary>
-	/// <param name="enemybullet"></param>
-	void AddEnemyBullet(EnemyBullet* enemybullet);
+	/// <param name="enemyBullet"></param>
+	void AddEnemyBullet(EnemyBullet* enemyBullet);
 
+	/// <summary>
+	/// 敵発生データの読みこみ
+	/// </summary>
 	void LoadEnemyPopDate();
 
+	/// <summary>
+	/// 敵コマンドの更新
+	/// </summary>
 	void UpdateEnemyPopCommands();
+
+	/// <summary>
+	/// 敵を生成する
+	/// </summary>
+	void AddEnemy(Vector3 pos);
+
+	// 敵弾リストのげったー
+	const std::list<EnemyBullet*>& GetBullet() { return enemybullets_; };
 
 private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
@@ -72,7 +87,7 @@ private: // メンバ変数
 	// モデル
 	Model* model_ = nullptr;
 
-	//スカイドーム用モデル
+	// スカイドーム用モデル
 	Model* modelSkydome_ = 0;
 
 	// ビュープロジェクション(ゲーム共通カメラ)
@@ -83,25 +98,27 @@ private: // メンバ変数
 
 	// 自分のキャラのデータ
 	Player* player_ = nullptr;
-	//敵のデータ
-	Enemy* enemy_ = nullptr;
-	//スカイドーム
-	Skydome* skydome_ = nullptr;
-
-
-	//敵の弾
-	// 弾(プレイヤーと同じようにリスト化しとく)
-	std::list<EnemyBullet*> enemybullets_;
-
-	//敵
+	// 敵のデータ(複数にする)
+	// Enemy* enemy_ = nullptr;
 	std::list<Enemy*> enemies_;
+
+	// スカイドーム
+	Skydome* skydome_ = nullptr;
 
 	// きゃめら
 	// デバッグカメラの挙動
 	bool isDebugCameraActive_ = false;
 	DebugCamera* debugCamera_ = nullptr;
 
-	//レールカメラのデータ
+	// レールカメラのデータ
 	RailCamera* railCamera_ = nullptr;
 
+	// 弾(プレイヤーと同じようにリスト化しとく)
+	std::list<EnemyBullet*> enemybullets_;
+
+	// 敵発生コマンド
+	std::stringstream enemyPopCommands;
+
+	int32_t waitTimer = 0;
+	bool waitFlag = false;
 };
