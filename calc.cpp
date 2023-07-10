a7add19a3b08ac68856cef67034e591682dc5c16
美鈴 武田 <k022g1076@m.neec.ac.jp>
2023-07-10 14:35:41 +0900

    Lerp関数作成

--- calc.cpp
+++ calc.cpp
@@ -18,3 +18,34 @@ Vector3 dir(const float& x, const float& y, const float& z) {
 
 	return dir;
 }
+
+Vector3 Lerp(const Vector3& v1, const Vector3& v2, float t) 
+{
+	//tempは資料のP?
+	Vector3 temp;
+
+	temp = Add(Multiply((1 - t), v1), Multiply(t,v2));
+
+	return temp;
+}
+
+
+// 加算
+Vector3 Add(const Vector3& v1, const Vector3& v2) {
+	Vector3 result;
+	result.x = v1.x + v2.x;
+	result.y = v1.y + v2.y;
+	result.z = v1.z + v2.z;
+
+	return result;
+};
+
+// スカラー倍
+Vector3 Multiply(float scalar, const Vector3& v) {
+	Vector3 result;
+	result.x = scalar * v.x;
+	result.y = scalar * v.y;
+	result.z = scalar * v.z;
+
+	return result;
+}
--- calc.h
+++ calc.h
@@ -8,3 +8,17 @@ Vector3 TransformNormal(const Vector3& v, const Matrix4x4& m);
 
 //正規化
 Vector3 dir(const float& x, const float& y, const float& z);
+
+/// <summary>
+/// Linear Interpolation　(線形保管)
+/// </summary>
+/// <param name="x3">スタート位置</param>
+/// <param name="v2">ゴール位置</param>
+/// <param name="t">フレーム(プレイヤーがどのぐらい移動しているか)</param>
+Vector3 Lerp(const Vector3& v1, const Vector3& v2, float t);
+
+// 加算
+Vector3 Add(const Vector3& v1, const Vector3& v2);
+
+// スカラー倍
+Vector3 Multiply(float scalar, const Vector3& v);
